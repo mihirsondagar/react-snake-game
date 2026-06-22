0db0a4fa81f3f10e4ce63ee5d819414d3a0fe234
Mihir Sondagar <mihirsondagar101@gmail.com>
2026-06-22 16:04:33 +0530

    Implemented Food Rendering and Handling Growth of Snake

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,13 +1,18 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { use, useEffect, useRef, useState } from "react";
 
 const blockSize = 30;
 
 const Board = () => {
   const boardRef = useRef(null);
+
   const [rows, setRows] = useState(0);
   const [cols, setCols] = useState(0);
 
   const [snake, setSnake] = useState([{ row: 1, col: 3 }]);
+
+  const directionRef = useRef("right");
+
+  const [food, setFood] = useState(null);
 
   useEffect(() => {
     const updateDimensions = () => {
@@ -39,11 +44,17 @@ const Board = () => {
         (segment) => segment.row === i && segment.col === j,
       );
 
+      let isFood = food && food.row === i && food.col === j;
+
       let elem = (
         <div
           key={`${i}-${j}`}
           style={{
-            backgroundColor: isSnake ? "limegreen" : "transparent",
+            backgroundColor: isSnake
+              ? "limegreen"
+              : isFood
+                ? "red"
+                : "transparent",
             border: "1px solid #333333",
             width: `${blockSize}px`,
             height: `${blockSize}px`,
@@ -56,11 +67,33 @@ const Board = () => {
   }
 
   useEffect(() => {
+    if (rows === 0 || cols === 0 || !food) return;
+
     const intervalId = setInterval(() => {
       setSnake((prev) => {
-        let head = { row: prev[0].row, col: prev[0].col + 1 };
+        let head;
+
+        let direction = directionRef.current;
+
+        if (direction === "up") {
+          head = { row: prev[0].row - 1, col: prev[0].col };
+        } else if (direction === "down") {
+          head = { row: prev[0].row + 1, col: prev[0].col };
+        } else if (direction === "right") {
+          head = { row: prev[0].row, col: prev[0].col + 1 };
+        } else if (direction === "left") {
+          head = { row: prev[0].row, col: prev[0].col - 1 };
+        }
+
         let newSnake = [head, ...prev];
-        newSnake.pop();
+
+        if (head.row === food.row && head.col === food.col) {
+          setFood(null);
+          spawnFood(rows, cols, newSnake);
+        } else {
+          newSnake.pop();
+        }
+
         return newSnake;
       });
     }, 200);
@@ -68,7 +101,54 @@ const Board = () => {
     return () => {
       clearInterval(intervalId);
     };
-  }, []);
+  }, [rows, cols, food]);
+
+  useEffect(() => {
+    const handleKey = (dets) => {
+      let direction = directionRef.current;
+
+      if (direction === "right" || direction === "left") {
+        if (dets.key === "ArrowUp") {
+          directionRef.current = "up";
+        } else if (dets.key === "ArrowDown") {
+          directionRef.current = "down";
+        }
+      } else if (direction === "up" || direction === "down") {
+        if (dets.key === "ArrowRight") {
+          directionRef.current = "right";
+        } else if (dets.key === "ArrowLeft") {
+          directionRef.current = "left";
+        }
+      }
+    };
+    window.addEventListener("keydown", handleKey);
+    return () => {
+      window.removeEventListener("keydown", handleKey);
+    };
+  }, [directionRef.current]);
+
+  const spawnFood = (currRows, currCols, currSnake) => {
+    let foodRow, foodCol;
+
+    let isOnSnake = true;
+
+    while (isOnSnake) {
+      foodRow = Math.floor(Math.random() * currRows);
+      foodCol = Math.floor(Math.random() * currCols);
+
+      isOnSnake = currSnake.some((segment) => {
+        return segment.row === foodRow && segment.col === foodCol;
+      });
+    }
+
+    setFood({ row: foodRow, col: foodCol });
+  };
+
+  useEffect(() => {
+    if (!food && rows > 0 && cols > 0) {
+      spawnFood(rows, cols, snake);
+    }
+  }, [rows, cols, food]);
 
   return (
     <div
